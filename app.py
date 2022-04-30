869517da84db4a340a74255ea94ec0d85f14bded
MatCuv <47555893+MatCuv@users.noreply.github.com>
2022-04-30 21:09:14 +0200

    Fonction pour détection d’un gagnant.

--- app.py
+++ app.py
@@ -12,3 +12,28 @@ app.config['SESSION_FILE_DIR'] = mkdtemp() # Stockage temporaire de la session, 
 app.config['SESSION_PERMANENT'] = False # Pas de stockage permanent
 app.config['SESSION_TYPE'] = 'filesystem' # Type de session
 Session(app) # Lancement de la session
+
+# JEU 
+combinaisons = [[[0,0], [0,1], [0,2]], #Lignes
+                [[1,0], [1,1], [1,2]],
+                [[2,0], [2,1], [2,2]],
+                [[0,0], [1,0], [2,0]], #Colonnes
+                [[0,1], [1,1], [2,1]],
+                [[0,2], [1,2], [2,2]],
+                [[0,0], [1,1], [2,2]], #Diagonales
+                [[0,2], [1,1], [2,0]]]
+
+def verif_gagnant(tableau):
+    # Renvoie X, O ou None respectivement si X a gagné dans le tableau, si O a gagné ou s'il n'y a pas de gagnant.'''
+    for combi in combinaisons:
+        nbX, nbO = 0, 0
+        for case in combi:
+            if tableau[case[0]][case[1]]=='X':
+                nbX += 1
+            elif tableau[case[0]][case[1]]=='O':
+                nbO += 1
+        if nbX == 3:
+            return 'X'
+        elif nbO == 3:
+            return 'O'
+    return 'Aucun'
