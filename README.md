0f7a400a10ec7ab628dd57bd42cfc5e90275d3a0
Imane Fares <imane@MacBook-Air-de-Imane.local>
2022-05-05 17:54:20 +0200

    Troisième stratégie de l'ordinateur (stratégie intelligente). Ajout à la page d'accueil. MAJ du readme.

--- README.md
+++ README.md
@@ -8,9 +8,20 @@ L'objectif de ce projet est de développer un jeu de morpion en python, que l'on
 Ce jeu sera disponible sur DockerHub en tant qu'image Docker et sur Heroku comme site web interactif.
 
 ### Stratégies 
+Le jeu pourra se faire à deux joueurs (qui joueront successivement) ou avec un joueur qui affrontera l'ordinateur selon 3 niveaux de difficultés (croissantes) :
+
 - **Niveau 1** : L'ordinateur choisit au hasard parmis les cases non sélectionnées. 
 
 - **Niveau 2** : Pour ce niveau de difficulté, si l’ordinateur est sur le point de gagner, alors il complète la ligne/colonne et gagne. Sinon, il regarde si le joueur est sur le point de gagner et si c’est le cas, il le bloque. Sinon, il choisit une position aléatoire parmi les cases vides restantes.
+
+- **Niveau 3** : Pour ce troisième niveau de difficulté, nous nous sommes inspirés des stratégies développées sur https://www.instructables.com/Winning-tic-tac-toe-strategies/. 
+    - Tout d’abord, si l’ordinateur est sur le point de gagner, alors il complète la ligne et gagne. Sinon, il regarde si le joueur est sur le point de gagner et si c’est le cas, il le bloque.
+    - Sinon, si l’ordinateur s’est déjà positionné sur 0 ou 1 case, alors il se place sur l’une des cases vides restantes en priorisant les coins, puis le centre, sinon il choisit une position aléatoire parmi les cases vides restantes.
+    - Si l’ordinateur s’est déjà positionné sur 2 cases ou plus, alors :
+        - S’il s‘est déjà positionné sur 2 coins et qu’un troisième est encore vide, il se place dessus
+        - Sinon, s’il s’est déjà positionné sur le centre ainsi que sur un coin et qu’un autre coin est encore vide, il se place dessus.
+        - Sinon, s’il s’est déjà positionné sur une case milieu de ligne et une case milieu de colonne, alors il se positionne sur la case adjacente ou sur le centre selon leur disponibilité
+    - Si aucune des précédentes stratégies n’a pu être appliquée, alors l’ordinateur se place sur l’une des cases vides restantes en priorisant les coins, puis le centre puis une case aléatoire.
 
 ## Organisation
 Organisation du répertoire
--- app.py
+++ app.py
@@ -25,6 +25,8 @@ combinaisons = [[[0,0], [0,1], [0,2]], #Lignes
                 [[0,0], [1,1], [2,2]], #Diagonales
                 [[0,2], [1,1], [2,0]]]
 
+coins = [[0,0], [0,2], [2,0], [2,2]]
+
 def verif_gagnant(tableau):
     # Renvoie X, O ou None respectivement si X a gagné dans le tableau, si O a gagné ou s'il n'y a pas de gagnant.'''
     for combi in combinaisons:
@@ -97,7 +99,123 @@ def bot_strategie_2(tableau):
 
 
 def bot_strategie_3(tableau):
-    return None
+    # Si on est sur le point de gagner, compléter la ligne
+    for position in combinaisons:
+        groupe = (tableau[position[0][0]][position[0][1]], 
+            tableau[position[1][0]][position[1][1]],
+            tableau[position[2][0]][position[2][1]])
+        if groupe.count('O') == 2 and groupe.count(None) == 1: 
+            indice_nouvellecase = groupe.index(None) 
+            return position[indice_nouvellecase]
+    # Si le joueur est sur le point de gagner, le bloquer     
+    for position in combinaisons:
+        groupe = (tableau[position[0][0]][position[0][1]], 
+                tableau[position[1][0]][position[1][1]],
+                tableau[position[2][0]][position[2][1]])
+        if groupe.count('X') == 2 and groupe.count(None) == 1: 
+            indice_nouvellecase = groupe.index(None) 
+            return position[indice_nouvellecase]
+    # Si on s'est déjà positionné sur 0 ou 1 case, on prend une nouvelle case avec en priorité les coins, puis le centre puis aléatoire sur le reste       
+    if tableau[0].count('O') + tableau[1].count('O') + tableau[2].count('O') <= 1 : 
+        for coin in coins:
+            if tableau[coin[0]][coin[1]]==None:
+                return coin
+        if tableau[1][1]==None:
+            return [1,1]
+        else:
+            return reponse_aleatoire(tableau)
+     
+    # Checker les cases vides restantes
+    cases_vides = []
+
+    for i in range(len(tableau)):
+        for j in range(len(tableau[i])):
+            if tableau[i][j]== None:
+                cases_vides.append([i,j])
+                
+    # Stratégie 1
+    # Si on s'est déjà positionné sur 2 cases ou plus
+    if tableau[0].count('O') + tableau[1].count('O') + tableau[2].count('O') > 1 : 
+        # Si on a les coins en haut à gauche et en haut à droite et si un des 2 autres coins est vide, on se place dessus
+        if tableau[coins[0][0]][coins[0][1]]=='O' and tableau[coins[1][0]][coins[1][1]]=='O':
+            if coins[2] in cases_vides:
+                return coins[2]
+            elif coins[3] in cases_vides:
+                return coins[3]
+        # Si on a les coins en haut à gauche et en bas à gauche et si un des 2 autres coins est vide, on se place dessus
+        elif tableau[coins[0][0]][coins[0][1]]=='O' and tableau[coins[2][0]][coins[2][1]]=='O':
+            if coins[1] in cases_vides:
+                return coins[1]
+            elif coins[3] in cases_vides:
+                return coins[3]
+        # Si on a les coins en haut à gauche et en bas à droite et si un des 2 autres coins est vide, on se place dessus
+        elif tableau[coins[0][0]][coins[0][1]]=='O' and tableau[coins[3][0]][coins[3][1]]=='O':
+            if coins[1] in cases_vides:
+                return coins[1]
+            elif coins[2] in cases_vides:
+                return coins[2]
+        # Si on a les coins en haut à droite et en bas à gauche et si un des 2 autres coins est vide, on se place dessus
+        elif tableau[coins[1][0]][coins[1][1]]=='O' and tableau[coins[2][0]][coins[2][1]]=='O':
+            if coins[0] in cases_vides:
+                return coins[0]
+            elif coins[3] in cases_vides:
+                return coins[3]
+        # Si on a les coins en haut à droite et en bas à droite et si un des 2 autres coins est vide, on se place dessus
+        elif tableau[coins[1][0]][coins[1][1]]=='O' and tableau[coins[3][0]][coins[3][1]]=='O':
+            if coins[0] in cases_vides:
+                return coins[0]
+            elif coins[2] in cases_vides:
+                return coins[2]        
+        # Si on a les coins en bas à gauche et en bas à droite et si un des 2 autres coins est vide, on se place dessus
+        elif tableau[coins[2][0]][coins[2][1]]=='O' and tableau[coins[3][0]][coins[3][1]]=='O':
+            if coins[0] in cases_vides:
+                return coins[0]
+            elif coins[1] in cases_vides:
+                return coins[1]  
+            
+        # Stratégie 2
+        # Si on a le centre et un coin et qu'un autre coin est libre, se placer dessus
+        for i in range(4):
+            if tableau[1][1]=='O' and tableau[coins[i][0]][coins[i][1]]=='O':
+                for j in range(4):
+                    if tableau[coins[j][0]][coins[j][1]]==None: 
+                        return coins[j]
+                    
+        # Stratégie 3 + 4
+        # Si on a la case (1,0) et la case (2,1) alors prendre (1,1) ou (2,0) si libres
+        if tableau[1][0]=='O' and tableau[2][1]=='O':
+            if tableau[1][1]==None:
+                return [1,1]
+            elif tableau[2][0]==None:
+                return [2,0]
+        # Si on a la case (2,1) et la case (1,2) alors prendre (1,1) ou (2,2) si libres
+        elif tableau[2][1]=='O' and tableau[1][2]=='O':
+            if tableau[1][1]==None:
+                return [1,1]
+            elif tableau[2][2]==None:
+                return [2,2]
+        # Si on a la case (1,2) et la case (0,1) alors prendre (1,1) ou (0,2) si libres
+        elif tableau[1][2]=='O' and tableau[0][1]=='O':
+            if tableau[1][1]==None:
+                return [1,1]
+            elif tableau[2][0]==None:
+                return [0,2]
+        # Si on a la case (0,1) et la case (1,0) alors prendre (1,1) ou (0,0) si libres
+        elif tableau[0][1]=='O' and tableau[1][0]=='O':
+            if tableau[1][1]==None:
+                return [1,1]
+            elif tableau[2][0]==None:
+                return [0,0]
+    
+        # Sinon, on complète en priorité les coins, puis le centre puis de manière aléatoire sur le reste
+        else:
+            for coin in coins:
+                if tableau[coin[0]][coin[1]]==None:
+                    return coin
+            if tableau[1][1]==None:
+                return [1,1]
+            else:
+                return bot_strategie_1(tableau)
 
 
 #JEU FLASK
